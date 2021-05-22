a625bf73d8a0cab5c7da50f0e649f0bb4e78d14b
HieuDepZaii <davidhieu1234@gmail.com>
2021-05-22 16:38:11 +0700

    update

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+
+
+const mysql = require('mysql');
+
+class Db {
+	constructor(config) {
+		this.connection = mysql.createPool({
+			connectionLimit: 100,
+			host: '127.0.0.1',
+			port: 3306,
+			user: 'root',
+			password: '1234',
+			database: 'nodejs_chat',
+			debug: false
+		});
+	}
+	query(sql, args) {
+		return new Promise((resolve, reject) => {
+			this.connection.query(sql, args, (err, rows) => {
+				if (err)
+					return reject(err);
+				resolve(rows);
+			});
+		});
+	}
+	close() {
+		return new Promise((resolve, reject) => {
+			this.connection.end(err => {
+				if (err)
+					return reject(err);
+				resolve();
+			});
+		});
+	}
+}
+module.exports = new Db();
